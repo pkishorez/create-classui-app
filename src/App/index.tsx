fe3526706d655fe2f23023ed38658cfb71795c82
Kishore <kishore.p@zen3.com>
2018-10-09 20:14:52 +0530

    Add material assets.

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import { ClassUI } from "classui/ClassUI";
 import { NavBar } from "classui/Navbar";
-
+import { Icon } from "classui/Helper/Icon";
 interface IProps {}
 export class App extends React.Component<IProps, any> {
 	render() {
 		return (
 			<ClassUI fullHeight>
 				<NavBar logo="Class-UI" />
+				<Icon>face</Icon>
 			</ClassUI>
 		);
 	}
